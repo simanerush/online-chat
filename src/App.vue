a1b29fcdd1dc74643dcdbbc0bffe4b37f2060106
Serafima <2002ssn@gmail.com>
2019-12-26 00:01:42 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <Container>
+      <ChatWindow @send-message="sendMessage">
+        <ChatMessage v-for="message in data" :key = "message.author" username="Ivan" time="21.12.2019 05:00:50">{{message.text}}</ChatMessage>
+      </ChatWindow>
+    </Container>
+  </div>
+</template>
+
+<script>
+import axios from axios
+import Container from './components/Container.vue'
+import ChatWindow from './components/ChatWindow.vue'
+import ChatMessage from './components/ChatMessage.vue'
+
+export default {
+  data() {
+    return {
+      data: []
+    }
+  },
+  methods: {
+    sendMessage(obj){
+        axios.post('http://188.225.47.187/api/chat/sendmessage.php')
+        author: obj.nickname;
+        text: obj.message;
+    },
+    getMessages(){
+      axios.get('http://188.225.47.187/api/chat/getmessages.php')
+      .then((response)=>{
+        let response = 
+        console.log('response',response.data)
+        this.data = response.data
+      })
+    }
+  },
+  mounted(){
+    setInterval(())=>{
+
+    })
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background-color: #f9f9fa;
+}
+</style>
---
+++ src/components/Container.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <slot></slot>
+    </div>
+</template>
+
+<style>
+    .container {
+        width: 764px;
+        margin: auto;
+        box-shadow: 0 0 15px 0 #5f5f5f73;
+    }
+</style>
+
